60e2a4cf1769addda301293c4aa1f170d33accb9
andyx0 <77806295+andyx0@users.noreply.github.com>
2023-03-13 17:26:04 -0400

    Added embeds

--- index.js
+++ index.js
@@ -1,165 +1,188 @@
 const djs = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 const client = new djs.Client({
-  intents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "DIRECT_MESSAGES"
-  ]
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "DIRECT_MESSAGES"
+    ]
 });
 const config = require('./config');
 const fetch = require("node-fetch");
 const myInit = {
-  method: 'GET',
-  headers: {
-    "Authorization": "Bearer " + config.cr_key,
-  },
-  mode: 'cors',
-  cache: 'default',
+    method: 'GET',
+    headers: {
+        "Authorization": "Bearer " + config.cr_key,
+    },
+    mode: 'cors',
+    cache: 'default'
 };
+const pfp = 'https://cdn.discordapp.com/avatars/958103047886766120/f9f7d78383a2a01e43f0b8f36eda4c52.png';
+let helpEmbed = new MessageEmbed()
+    .setAuthor({ name: 'HEHEHEHA', iconURL: pfp })
+    .setTitle("Command List")
+    .setDescription('c!spy, c!stop, c!list')
+    .setThumbnail(pfp)
+    .addFields(
+        { name: '\u200B', value: '\u200B' },
+        { name: 'c!spy', value: 'Add a player to your watchlist: `c!spy <tag>`', inline: true },
+        { name: 'c!stop', value: 'Stop watching a player: `c!stop <tag>`', inline: true },
+        { name: 'c!list', value: 'List your current targets', inline: true },
+    )
+    .setFooter({ text: 'HEHEHEHA', iconURL: pfp });
+let listEmbed = new MessageEmbed()
+    .setColor(0x0099FF)
+    .setAuthor({ name: 'HEHEHEHA', iconURL: pfp })
+    .setTitle("Current Watchlist")
+    .setThumbnail(pfp)
+    .setFooter({ text: 'HEHEHEHA', iconURL: pfp });
 const db = require('./db');
 
 async function spy(player_tag) {
-  let cooldown = 60000; // 1 min cooldown default
-  if (await checkTarget(player_tag))
-    cooldown = 1800000; // half hour cooldown if target played recently
-  console.log(`Cooldown (ms) for ${player_tag}: ${cooldown}`);
-  setTimeout(spy, cooldown, player_tag);
+    let cooldown = 60000; // 1 min cooldown default
+    if (await checkTarget(player_tag)) {
+        cooldown = 1800000; // half hour cooldown if target played recently
+    }
+    console.log(`Cooldown (ms) for ${player_tag}: ${cooldown}`);
+    setTimeout(spy, cooldown, player_tag);
 }
 const player_tags = db.getPlayerTags();
-for (let i = 0; i < player_tags.length; i++)
-  setTimeout(spy, 5000, player_tags[i]); // wait 5 seconds on startup
+for (let i = 0; i < player_tags.length; i++) {
+    setTimeout(spy, 5000, player_tags[i]); // wait 5 seconds on startup
+}
 
 async function checkTarget(target) {
-  let found = false;
-  let minuteDiff = 5;
-  let json;
-  try {
-    json = await fetch("https://api.clashroyale.com/v1/players/%23" + target + "/battlelog", myInit).then(safeParseJSON);
-  } catch (err) {
-    return true;
-  }
-  if (json.length > 0) {
-    minuteDiff = getMinDiff(json);
-    console.log(`Minute difference for ${target}: ${minuteDiff}`);
-  }
-  // Message watchers if target played recently
-  if (minuteDiff < 5) {
-    let ign;
+    let found = false;
+    let minuteDiff = 5;
+    let json;
     try {
-      ign = await getPlayerName(target);
+        json = await fetch("https://api.clashroyale.com/v1/players/%23" + target + "/battlelog", myInit).then(safeParseJSON);
+        if (json.length > 0) {
+            minuteDiff = getMinDiff(json);
+            console.log(`Minute difference for ${target}: ${minuteDiff}`);
+        }
+        // Message watchers if target played recently
+        if (minuteDiff < 5) {
+            let ign;
+            ign = await getPlayerName(target);
+            const msg = `${ign} (${target}) has played a ${json[0]["type"]} match in the last 5 minutes!`;
+            console.log(msg);
+            const users = db.getWatchers(target);
+            for (let x = 0; x < users.length; x++) {
+                let user;
+                user = await client.users.fetch(users[x]);
+                if (!user) {
+                    console.log("User not found");
+                    return true;
+                }
+                user.send(msg).catch(() => {
+                    console.log("User has DMs closed or no mutual servers with the bot");
+                    return;
+                });
+            }
+            found = true;
+        }
     } catch (err) {
-      return true;
-    }
-    const msg = `${ign} (${target}) has played a ${json[0]["type"]} match in the last 5 minutes!`;
-    console.log(msg);
-    const users = db.getWatchers(target);
-    for (let x = 0; x < users.length; x++) {
-      let user;
-      try {
-        user = await client.users.fetch(users[x]);
-      } catch (err) {
         return true;
-      }
-      if (!user) {
-        console.log("User not found");
-        return true;
-      }
-      user.send(msg).catch(() => {
-        console.log("User has DMs closed or no mutual servers with the bot");
-        return;
-      });
     }
-    found = true;
-  }
-  return found;
+    return found;
 }
 
 async function safeParseJSON(response) {
-  const body = await response.text();
-  try {
-    return JSON.parse(body);
-  } catch (err) {
-    console.error("Error:", err);
-    console.error("Response body:", body);
-    // throw err
-    return ReE(response, err.message, 500);
-  }
+    const body = await response.text();
+    try {
+        return JSON.parse(body);
+    } catch (err) {
+        console.error("Error:", err);
+        console.error("Response body:", body);
+        // throw err
+        return ReE(response, err.message, 500);
+    }
 }
 
 function getMinDiff(json) {
-  var battleTime = json[0]["battleTime"];
-  var present = new Date();
-  var year = parseInt(battleTime.substring(0, 4));
-  var month = parseInt(battleTime.substring(4, 6)) - 1;
-  var day = parseInt(battleTime.substring(6, 8));
-  var hour = parseInt(battleTime.substring(9, 11)) - 4;
-  var minute = parseInt(battleTime.substring(11, 13));
-  var second = parseInt(battleTime.substring(13, 15));
-  var lastBattle = new Date(year, month, day, hour, minute, second);
-  // console.log("Current time:", present);
-  // console.log("Last battle time:", lastBattle);
-  minuteDiff = (present - lastBattle) / 1000; // timeDiff in seconds
-  return minuteDiff / 60.0; // timeDiff in minutes
+    var battleTime = json[0]["battleTime"];
+    var present = new Date();
+    var year = parseInt(battleTime.substring(0, 4));
+    var month = parseInt(battleTime.substring(4, 6)) - 1; // account for 0 indexed months
+    var day = parseInt(battleTime.substring(6, 8));
+    var hour = parseInt(battleTime.substring(9, 11)) - 5;
+    var minute = parseInt(battleTime.substring(11, 13));
+    var second = parseInt(battleTime.substring(13, 15));
+    var lastBattle = new Date(year, month, day, hour, minute, second);
+    // console.log("Current time:", present);
+    // console.log("Last battle time:", lastBattle);
+    minuteDiff = (present - lastBattle) / 1000; // timeDiff in seconds
+    return minuteDiff / 60.0; // timeDiff in minutes
 }
 
 async function getPlayerName(playerTag) {
-  const playerJson = await fetch("https://api.clashroyale.com/v1/players/%23" + playerTag, myInit).then(safeParseJSON);
-  return playerJson.name;
+    const playerJson = await fetch("https://api.clashroyale.com/v1/players/%23" + playerTag, myInit).then(safeParseJSON);
+    return playerJson.name;
 }
 
 client.on('ready', () => {
-  client.user.setActivity("Clash Royale players", { type: "WATCHING" });
-  console.log(client.user.username, 'is online.');
+    client.user.setActivity("Clash Royale players", { type: "WATCHING" });
+    console.log(client.user.username, 'is online.');
 });
 
 client.on('messageCreate', async (msg) => {
-  if (!msg.content.startsWith(config.prefix) || msg.author.bot)
-    return;
-  // if (msg.author.bot || msg.channel.type === "dm")
-  //   return;
-  const messageArray = msg.content.split(' ');
-  const cmd = messageArray[0].substring(2);
-  const targetTag = msg.content.substring(msg.content.indexOf(' ') + 1).toUpperCase();
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+    // if (msg.author.bot || msg.channel.type === "dm")
+    //   return;
+    const messageArray = msg.content.split(' ');
+    const cmd = messageArray[0].substring(2);
+    let arg1 = msg.content.substring(msg.content.indexOf(' ') + 1).toUpperCase();
 
-  switch (cmd) {
-    case 'ping':
-      msg.channel.send('pong!');
-      break;
-    case 'help': // todo
-      msg.channel.send("My prefix is `c!`. Type `c!spy <tag>` to watch a player, `c!stop <tag>` to stop watching a player, and `c!list` to see your current targets!");
-      break;
-    case 'spy':
-      const playerName = await getPlayerName(targetTag);
-      const validPlayerTag = (playerName != null);
-      if (validPlayerTag) {
-        const playerTags = db.getPlayerTags();
-        added = db.addWatcher(targetTag, msg.author.id);
-        if (added) {
-          msg.channel.send(`New target ${playerName} (${targetTag}) added!`);
-          if (!playerTags.includes(targetTag))
-            spy(targetTag);
-        } else {
-          msg.channel.send(`Target ${playerName} (${targetTag}) already tracked!`);
-        }
-      } else {
-        msg.channel.send("Invalid player tag!");
-      }
-      break;
-    case 'stop':
-      deleted = db.deleteWatcher(targetTag, msg.author.id);
-      if (deleted)
-        msg.channel.send(`Tag ${targetTag} deleted!`);
-      else
-        msg.channel.send(`Tag ${targetTag} is not tracked!`);
-      break;
-    case 'list':
-      targets = db.getTargets(msg.author.id);
-      if (targets.length > 0)
-        msg.channel.send(targets.join(", "));
-      else
-        msg.channel.send("You have no targets!");
-      break;
-  }
+    switch (cmd) {
+        case 'ping':
+            msg.channel.send('pong!');
+            break;
+        case 'help':
+            helpEmbed.setColor("RANDOM").setTimestamp();
+            // msg.channel.send("My prefix is `c!`. Type `c!spy <tag>` to watch a player, `c!stop <tag>` to stop watching a player, and `c!list` to see your current targets!");
+            msg.channel.send({ embeds: [helpEmbed] });
+            break;
+        case 'spy':
+            if (arg1.charAt(0) == '#') {
+                arg1 = arg1.substring(1);
+            }
+            const playerName = await getPlayerName(arg1);
+            const validPlayerTag = (playerName != null);
+            if (validPlayerTag) {
+                const playerTags = db.getPlayerTags();
+                added = db.addWatcher(arg1, msg.author.id);
+                if (added) {
+                    msg.channel.send(`New target ${playerName} (${arg1}) added!`);
+                    if (!playerTags.includes(arg1)) {
+                        spy(arg1);
+                    }
+                } else {
+                    msg.channel.send(`Target ${playerName} (${arg1}) already tracked!`);
+                }
+            } else {
+                msg.channel.send("Invalid player tag! Usage: `c!spy <tag>`");
+            }
+            break;
+        case 'stop':
+            deleted = db.deleteWatcher(arg1, msg.author.id);
+            if (deleted) {
+                msg.channel.send(`Tag ${arg1} deleted!`);
+            } else {
+                msg.channel.send(`Tag ${arg1} is not tracked!`);
+            }
+            break;
+        case 'list':
+            targets = db.getTargets(msg.author.id);
+            if (targets.length > 0) {
+                listEmbed.setDescription(targets.join(", ")).setTimestamp();
+                msg.channel.send({ embeds: [listEmbed] });
+                // msg.channel.send(targets.join(", "));
+            } else {
+                msg.channel.send("You have no targets!");
+            }
+            break;
+    }
 });
 
 client.login(config.token);
